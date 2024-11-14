704ef09eddf080206653fc655a9396d31f1f95f1
ee-bit <zombiee256@gmail.com>
2024-11-14 18:53:14 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('um usuário conectado');
+    socket.on('disconnect', () => {
+        console.log('usuário desconectado');
+    });
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
